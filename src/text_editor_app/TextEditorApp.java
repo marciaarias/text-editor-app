709533d35a10ddda490612d7b73ac4b90c0b662f
Marcia Arias <arias.ca@hotmail.com>
2019-05-20 20:55:18 -0400

    Implement menuItems Cut,Copy,Paste,WordWrap

--- src/text_editor_app/TextEditorApp.java
+++ src/text_editor_app/TextEditorApp.java
@@ -7,14 +7,18 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
+import javax.swing.undo.UndoManager;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JCheckBoxMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TextEditorApp {
 
 	private JFrame frmTextEditor;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -78,23 +82,41 @@ public class TextEditorApp {
 		JMenu menuEdit = new JMenu("Edit");
 		menuBar.add(menuEdit);
 		
-		JMenuItem menuItemUndo = new JMenuItem("Undo");
-		menuEdit.add(menuItemUndo);
-		
-		JMenuItem menuItemRedo = new JMenuItem("Redo");
-		menuEdit.add(menuItemRedo);
+		//Implement menuItem "Cut".
 		
 		JMenuItem menuItemCut = new JMenuItem("Cut");
+		menuItemCut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textArea.cut();
+				
+			}
+		});
 		menuEdit.add(menuItemCut);
 		
+		//Implement menuItem "Copy".
+		
 		JMenuItem menuItemCopy = new JMenuItem("Copy");
+		menuItemCopy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textArea.copy();
+				
+			}
+		});
 		menuEdit.add(menuItemCopy);
 		
-		JMenuItem menuItemPaste = new JMenuItem("Paste");
-		menuEdit.add(menuItemPaste);
+		//Implement menuItem "Paste".
 		
-		JMenuItem menuItemDelete = new JMenuItem("Delete");
-		menuEdit.add(menuItemDelete);
+		JMenuItem menuItemPaste = new JMenuItem("Paste");
+		menuItemPaste.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textArea.paste();
+				
+			}
+		});
+		menuEdit.add(menuItemPaste);
 		
 		menuEdit.add(new JSeparator());
 		
@@ -112,7 +134,20 @@ public class TextEditorApp {
 		JMenu menuFormat = new JMenu("Format");
 		menuBar.add(menuFormat);
 		
+		//Implement menuItem "Word Wrap".
+		
 		JCheckBoxMenuItem chckbxMenuItemWordWrap = new JCheckBoxMenuItem("Word Wrap");
+		chckbxMenuItemWordWrap.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(chckbxMenuItemWordWrap.isSelected() == true) {
+					textArea.setLineWrap(true);
+				} else {
+					textArea.setLineWrap(false);
+				}
+				
+			}
+		});
 		menuFormat.add(chckbxMenuItemWordWrap);
 		
 		JMenuItem menuItemFont = new JMenuItem("Font...");
@@ -122,7 +157,7 @@ public class TextEditorApp {
 		scrollPane.setBounds(3, 23, 498, 617);
 		frmTextEditor.getContentPane().add(scrollPane);
 		
-		JTextArea textArea = new JTextArea();
+		textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
 	}
 }
